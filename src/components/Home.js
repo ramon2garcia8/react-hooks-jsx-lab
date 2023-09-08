6214ce8c71fc22388446e3dac811030f8da15dbe
Ramon A. Garcia <ramon2garcia8@gmail.com>
2023-09-08 10:00:25 -0400

    Added assignments

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,10 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import { name, city } from "../data/data.js";
 
 function Home() {
+  console.log(name)
   // update the JSX being returned!
-  return <div>Home</div>;
+  return <div id="home"><h1 style={{color: "firebrick"}}>{name} is a Web Developer from {city}</h1></div>;
 }
 
 export default Home;
